0df8f3c95d741c327cf5479d4e8729914568c1f8
Jeffrey Hing <hingcruz@gmail.com>
2015-07-06 18:59:28 -0700

    Add accordion example.

---
+++ src/examples/accordionDirective/accordionDirective.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015. Author: Jeffrey Hing. All Rights Reserved.
+ *
+ * MIT License
+ *
+ * Accordion directive.
+ */
+'use strict';
+
+//--------------------------------------
+// Module dependencies and variables
+//--------------------------------------
+
+var cabinetDirective = require('angular-cabinet-directive');
+
+require('./accordionDirective.css');
+
+//--------------------------------------
+// Module exports
+//--------------------------------------
+
+module.exports = function(moduleName) {
+
+    cabinetDirective(moduleName, {
+        directiveNames: {
+            cabinet: 'accordion',
+            drawerTrigger: 'accordionTrigger',
+            drawerContents: 'accordionContents',
+            drawerClass: 'accordionClass'
+        }
+    });
+};
